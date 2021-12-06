31b2d1ca0b4f18a8571b32aa739d4b69b4b14a08
Adam Foster <adamfoster@gmail.com>
2021-12-06 19:49:10 +1100

    Template with demo code

---
+++ template.jl
@@ -0,0 +1,10 @@
+using OffsetArrays
+
+demo = false
+
+open(demo ? "dayX/demoinput" : "dayX/input", "r") do f # demoinput
+    line = readline(f)
+    while line != ""
+        line = readline(f)
+    end
+end
