9e56564b653e1253e03f3f3d8d7e6a8045c7d53a
Adam Foster <adamfoster@gmail.com>
2021-12-21 09:53:00 +1100

    Day 16?

--- day16/day16part2.jl
+++ day16/day16part2.jl
@@ -15,7 +15,6 @@ function readheader(bits) # 6 bits
 end
 
 function readliteral(bits, base) # unknown bits
-    startbase = base
     lastgroup = false
     value = 0
     while !lastgroup
@@ -28,9 +27,7 @@ function readliteral(bits, base) # unknown bits
         end
         base += 5
     end
-    #while (base-startbase)%4 != 1
-    #    base += 1
-    #end
+
     return (value, base)
 end
 
@@ -40,8 +37,19 @@ end
 
 demo = false
 
-open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
+open(demo ? "day16/demoinput1" : "day16/input", "r") do f # demoinput
     line = readline(f)
+
+    #test data:
+    #line = "C200B40A82" # 3
+    #line = "04005AC33890" # 54
+    #line = "880086C3E88112" # 7
+    #line = "CE00C43D881120" # 9
+    #line = "D8005AC2A8F0" # 1
+    #line = "F600BC2D8F" # 0
+    #line = "9C005AC2F8F0" # 0 
+    #line = "9C0141080250320F1802104A08" # 1
+
     bits = Vector{Bool}()
     for hexdigit in split(line, "")
         append!(bits, digits(parse(Int8, hexdigit, base=16), base=2, pad=4) |> reverse)
@@ -59,10 +67,6 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
     i = 1
     done = false
     while !done
-        #top = nothing
-        #if size(stack,1) != 0
-        #    top = last(stack)
-        #end
         oldi = i
 
         # Read header
@@ -75,23 +79,26 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
             # literal
             value, i = readliteral(bits, i)
             println("Literal = $value")
+            if size(stack, 1) != 0
+                println("Adding value $value to " * string(size(stack, 1)))
+                top = last(stack)
+                push!(top["subvalues"], value)
+            end
 
             #check if can close a packet
             popping = true
             while popping
                 popping = false
-                if size(stack, 1) == 0
-                    println("Done!")
-                    break
-                end
+                popped = nothing
+                
                 top = last(stack)
                 if "length" in keys(top)
                     if i == top["finish"]
-                        pop!(stack)
+                        popped = pop!(stack)
                         popping = true
                     elseif i > top["finish"]
                         println("Why is $i > " * string(top["finish"]) * ". This better be the last packet!")
-                        pop!(stack)
+                        popped = pop!(stack)
                         popping = true
                     elseif demo
                         println("At $i - need to get to " * string(top["finish"]) * " at stack " * string(size(stack,1)))
@@ -99,11 +106,48 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
                 else # must be a count type
                     top["seen"] += 1
                     if top["seen"] == top["packets"]
-                        pop!(stack)
+                        popped = pop!(stack)
                         popping = true
                     elseif demo
                         println("At " * string(top["seen"]) * " - need to get to " * string(top["packets"]) * " at stack " * string(size(stack,1)))
                     end
+                end
+
+                if popped !== nothing
+                    poppedvalue = -1
+                    if popped["type"] == 0
+                        poppedvalue = reduce(+, popped["subvalues"])
+                    elseif popped["type"] == 1
+                        poppedvalue = reduce(*, popped["subvalues"])
+                    elseif popped["type"] == 2
+                        poppedvalue = reduce(min, popped["subvalues"])
+                    elseif popped["type"] == 3
+                        poppedvalue = reduce(max, popped["subvalues"])
+                    elseif popped["type"] == 5
+                        v2 = pop!(popped["subvalues"])
+                        v1 = pop!(popped["subvalues"])
+                        poppedvalue = v1 > v2 ? 1 : 0
+                    elseif popped["type"] == 6
+                        v2 = pop!(popped["subvalues"])
+                        v1 = pop!(popped["subvalues"])
+                        poppedvalue = v1 < v2 ? 1 : 0
+                    elseif popped["type"] == 7
+                        v2 = pop!(popped["subvalues"])
+                        v1 = pop!(popped["subvalues"])
+                        poppedvalue = v1 == v2 ? 1 : 0
+                    else
+                        println("*** Unknown type id! ***")
+                    end
+                    println("Pushing poppedvalue $poppedvalue up the stack")
+                    if size(stack, 1) == 0
+                        println("Done!")
+                        println(poppedvalue)
+                        break
+                    else
+                        nexttop = last(stack)
+                        push!(nexttop["subvalues"], poppedvalue)
+                    end
+
                 end
             end
 
@@ -113,7 +157,7 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
                 # 15 bits are a number that represents the total length in bits of the sub-packets
                 sublength = todecimal(bits[i+1:i+15])
                 i += 16
-                push!(stack, Dict("length"=>sublength, "start"=>i, "finish"=>(sublength+i), "version"=>nextversion, "type"=>nexttypeid)) # [0, sublength, i])
+                push!(stack, Dict("length"=>sublength, "start"=>i, "finish"=>(sublength+i), "version"=>nextversion, "type"=>nexttypeid, "subvalues"=>[])) # [0, sublength, i])
                 if sublength > size(bits, 1)
                     println("************ - length")
                     display(stack)
@@ -124,7 +168,7 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
                 # 11 bits are a number that represents the number of sub-packets
                 subcount = todecimal(bits[i+1:i+11])
                 i += 12
-                push!(stack, Dict("packets"=>subcount, "start"=>i, "seen"=>0, "version"=>nextversion, "type"=>nexttypeid)) #[1, subcount, i])
+                push!(stack, Dict("packets"=>subcount, "start"=>i, "seen"=>0, "version"=>nextversion, "type"=>nexttypeid, "subvalues"=>[])) #[1, subcount, i])
                 if subcount > 1000
                     println("************ - count")
                     display(stack)
@@ -145,6 +189,6 @@ open(demo ? "day16/demoinput4" : "day16/input", "r") do f # demoinput
     #println()
     #nextversion, nexttypeid = readheader(bits[1:6])
     
-    println("i = $i")
-    println("totalversions = $totalversions") # 920
+    #println("i = $i")
+    #println("totalversions = $totalversions")
 end
